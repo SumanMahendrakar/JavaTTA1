bea1db826cfe0c4f71f1feac719552fc83e000b0
Suman K Mahendrakar <sumankmahendrakar@gmail.com>
2024-10-08 19:45:04 +0530

    My files

---
+++ src/Github_Package/Pgrm01.java
@@ -0,0 +1,11 @@
+package Github_Package;
+
+import javax.swing.*;
+
+public class Pgrm01
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Prgrm 1 to push into Github..!!!");
+    }
+}
---
+++ src/OCT_Tasks10/Lab_03_03102024.java
@@ -0,0 +1,16 @@
+package OCT_Tasks10;
+
+import java.io.PrintStream;
+
+public class Lab_03_03102024
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Simple Calculator using SOP Statements");
+        System.out.println("--------------------------------------");
+        System.out.println("Addition of 2 numbers = 10+20=30");
+        System.out.println("Subtraction of 2 numbers = 20-10=10");
+        System.out.println("Multiplication of 3 numbers = 3*4*5=60");
+        System.out.println("Division of 2 numbers = 100/5=20");
+    }
+}
