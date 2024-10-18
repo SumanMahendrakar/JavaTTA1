6fa87fb7f344489c77e4279f96e8bd2ee1fabdae
Suman K Mahendrakar <sumankmahendrakar@gmail.com>
2024-10-18 08:29:13 +0530

    Variables

---
+++ src/OCT_Tasks10/Lab_05_08102024.java
@@ -0,0 +1,14 @@
+package OCT_Tasks10;
+
+public class Lab_05_08102024
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Data types and Variables");
+        //Data_type Variable_name / Identifier = Variable_value / literals
+        int age = 20;
+        int age1 = 44;
+        System.out.println(age1);
+
+    }
+}
